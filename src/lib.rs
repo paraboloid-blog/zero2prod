8955c7ea8035def86720bf68c4b7fc0d4d2cecea
box-for-spam <box-for-spam@protonmail.ch>
2025-03-31 07:38:09 +0200

    Chapter 4

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+pub mod telemetry;
+
+#[cfg(test)]
+mod tests {
+    use crate::routes::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
+}
