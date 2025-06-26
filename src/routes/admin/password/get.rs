a90187d2806c8bca1cc39f62393b4ff737eef3f8
paraboloid <box-for-spam@protonmail.ch>
2025-06-26 21:50:59 +0200

    Page 507

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -2,20 +2,33 @@ use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
+use actix_web_flash_messages::IncomingFlashMessages;
+use std::fmt::Write;
 
-pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password_form(
+    session: TypedSession,
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
     };
 
-    Ok(HttpResponse::Ok().content_type(ContentType::html()).body(
-        r#"<!DOCTYPE html>
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>Change Password</title>
 </head>
 <body>
+{msg_html}
 <form action="/admin/password" method="post">
 <label>Current password
 <input type="password" placeholder="Enter current password" name="current_password">
@@ -34,5 +47,5 @@ pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse,
 <p><a href="/admin/dashboard">&lt;- Back</a></p>
 </body>
 </html>"#,
-    ))
+        )))
 }
