26a8ddaf8e85774dc849953e5e537103b193e643
paraboloid <box-for-spam@protonmail.ch>
2025-06-27 20:10:10 +0200

    Page 524

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,7 +3,6 @@ mod greet;
 mod health_check;
 mod home;
 mod login;
-mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
 
@@ -12,6 +11,5 @@ pub use greet::*;
 pub use health_check::*;
 pub use home::*;
 pub use login::*;
-pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
