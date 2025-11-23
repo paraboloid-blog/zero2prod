e79e64449a9d7bd1626d154dc2d38f7208d56b7c
paraboloid <box-for-spam@protonmail.ch>
2025-11-23 09:34:53 +0100

    Test

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -19,8 +19,7 @@ APP_USER_PWD="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]
-then
+if [[ -z "${SKIP_DOCKER}" ]]; then
   # if a postgres container is running, print instructions to kill it and exit
   RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
   if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
@@ -31,30 +30,30 @@ then
   CONTAINER_NAME="postgres_$(date '+%s')"
   # Launch postgres using Docker
   docker run \
-      --env POSTGRES_USER=${SUPERUSER} \
-      --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
-      --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-      --health-interval=1s \
-      --health-timeout=5s \
-      --health-retries=5 \
-      --publish "${DB_PORT}":5432 \
-      --detach \
-      --name "${CONTAINER_NAME}" \
-      postgres -N 1000
-      # ^ Increased maximum number of connections for testing purposes
-      
+    --env POSTGRES_USER=${SUPERUSER} \
+    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres -N 1000
+  # ^ Increased maximum number of connections for testing purposes
+
   until [ \
     "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
     "healthy" \
-  ]; do     
+    ]; do
     >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1 
+    sleep 1
   done
-  
+
   # Create the application user
   CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
   docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
-  
+
   # Grant create db privileges to the app user
   GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
   docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
@@ -67,5 +66,7 @@ DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_D
 export DATABASE_URL
 sqlx database create
 sqlx migrate run
+
+ulimit -n 65535
 
 >&2 echo "Postgres has been migrated, ready to go!"
